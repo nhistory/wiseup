1e996c5f8d3de2a5324d89a87b99415cc9ef3c4d
Sehwan <sehwan.lee.dev@gmail.com>
2025-12-25 17:38:16 -0400

    feat: Migrate styling to Tailwind CSS by updating configurations, dependencies, and applying utility classes to components.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://sehwanlee.github.io",
   base: "/wiseup",
   output: "static",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
